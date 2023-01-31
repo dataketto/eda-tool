1bc0608cbaa36603bc03ce0c00e1913435deb882
dataketto <123143985+dataketto@users.noreply.github.com>
2023-01-31 21:04:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ def upload():
             file_selected_1 = st.selectbox("Select file",file_names)
             st.write(file_selected_1)
             file_selected = file_path+file_selected_1
+            file_selected = st.text_input("Paste a Folder Path  D:\Download\ ) ")
             if file_selected is not None:
                 if ".xlsx" in file_selected:
                     data_xls = pd.read_excel(f"{file_selected}", index_col=None)
